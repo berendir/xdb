162ea170ce06f6078639c072d9130e04d8adf952
Berendir Huntinghawk <berendir.huntinghawk@gmail.com>
2017-09-29 10:20:06 +0200

    Add db3 field and record implementations

---
+++ src/db3_field.h
@@ -0,0 +1,72 @@
+/*******************************************************************************
+** MIT License
+**
+** Copyright (c) 2017 Berendir Huntinghawk
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to
+** deal in the Software without restriction, including without limitation the
+** rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+** sell copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+** IN THE SOFTWARE.
+*******************************************************************************/
+
+#ifndef XDB_DB3_FIELD_H
+#define XDB_DB3_FIELD_H
+
+#include <memory>
+#include "field_base.h"
+#include "db3_structures.h"
+
+
+namespace xdb {
+
+union db3_field_data {
+    std::string *string;
+    std::tm *date;
+    double number;
+    bool boolean;
+};
+
+class db3_field : public field_base
+{
+    friend class db3_record;
+
+public:
+    ~db3_field();
+
+    std::string name() const;
+
+    field_type type() const;
+
+    std::string toString() const;
+
+    std::tm toDate() const;
+
+    bool toBool() const;
+
+    double toNumber() const;
+
+protected:
+    db3_field(db3_field_descriptor *descriptor);
+
+    void set_value(const char *data, int length);
+
+    db3_field_descriptor *m_descriptor;
+    db3_field_data m_data;
+};
+
+}
+
+#endif // XDB_DB3_FIELD_H
---
+++ src/db3_record.h
@@ -0,0 +1,61 @@
+/*******************************************************************************
+** MIT License
+**
+** Copyright (c) 2017 Berendir Huntinghawk
+**
+** Permission is hereby granted, free of charge, to any person obtaining a copy
+** of this software and associated documentation files (the "Software"), to
+** deal in the Software without restriction, including without limitation the
+** rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+** sell copies of the Software, and to permit persons to whom the Software is
+** furnished to do so, subject to the following conditions:
+**
+** The above copyright notice and this permission notice shall be included in
+** all copies or substantial portions of the Software.
+**
+** THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+** IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+** FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+** AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+** LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+** FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+** IN THE SOFTWARE.
+*******************************************************************************/
+
+#ifndef DB3_RECORD_H
+#define DB3_RECORD_H
+
+#include <vector>
+#include <memory>
+#include <unordered_map>
+#include "record_base.h"
+#include "db3_structures.h"
+#include "db3_field.h"
+
+
+namespace xdb {
+
+class db3_record : public record_base
+{
+    friend class db3_table;
+
+public:
+    ~db3_record();
+
+    virtual field_base * field(int index) const;
+
+    virtual field_base * field(const std::string &name) const;
+
+protected:
+    db3_record(std::shared_ptr<std::vector<db3_field_descriptor>> descriptors);
+
+    void set_value(const char *data, int length);
+
+    std::shared_ptr<std::vector<db3_field_descriptor>> m_descriptors;
+    std::vector<std::unique_ptr<db3_field>> m_fields;
+    std::unordered_map<std::string,int> m_field_indexes;
+};
+
+}
+
+#endif // DB3_RECORD_H
